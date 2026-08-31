6794a156012033b128a2f29a9e7f6d7bd4850aab
heart-breaker666 <2992077207@qq.com>
2026-08-31 19:02:25 +0800

    Revert "add show source"
    This reverts commit 94930ad88accfb614cf16381391c738742b9cefc.

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -22,9 +22,9 @@ const int kMaxEvents = 1024;
 constexpr int kTimerTimeoutSec = 15;
 
 // MySQL 连接信息（与本地环境一致）
-const char* kDbUrl = "ip";
+const char* kDbUrl = "127.0.0.1";
 const char* kDbUser = "root";
-const char* kDbPassword = "password";
+const char* kDbPassword = "fxh668";
 const char* kDbName = "yourdb";
 const int kDbPort = 3306;
 
